b7f74b7bb648206cca30c9b4048a63a0630a8fd8
HaeJun <haejunbag131@gmail.com>
2026-05-26 09:15:49 +0900

    Chore: 백엔드 세팅

---
+++ src/main/java/com/example/demo/MoodwaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoodwaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodwaveApplication.class, args);
+	}
+
+}
