5a7eaa7004ff46c792a699aed68afe6139252b2c
DongHYEON <85489985+donghyeon01@users.noreply.github.com>
2026-06-01 16:17:45 +0900

    FIx : 병합 오류 수정

--- src/main/java/com/example/demo/mapper/LikeMapper.java
+++ src/main/java/com/example/demo/mapper/LikeMapper.java
@@ -8,51 +8,17 @@ import java.util.List;
 @Mapper
 public interface LikeMapper {
 
-    @Select("""
-        SELECT
-            spotify_id AS spotifyId,
-            music_id AS musicId
-        FROM likes
-        WHERE spotify_id = #{spotifyId}
-    """)
-    List<LikeVO> findLike(@Param("spotifyId") String spotifyId);
+    // 1. 가입된 회원인지 확인하기 위해 Spotify ID로 조회
+    @Select("SELECT * FROM liked WHERE spotify_id = #{spotifyId}")
+    List<LikeVO> findLike(String spotifyId);
 
-    @Select("""
-        SELECT
-            spotify_id AS spotifyId,
-            music_id AS musicId
-        FROM likes
-        WHERE spotify_id = #{spotifyId}
-          AND music_id = #{musicId}
-    """)
-    LikeVO isLike(
-            @Param("spotifyId") String spotifyId,
-            @Param("musicId") String musicId
-    );
+    @Select("SELECT * FROM liked WHERE spotify_id = #{spotifyId} AND music_id = #{musicId}")
+    LikeVO isLike(String spotifyId,String musicId);
             
     // 2. 처음 로그인한 사용자라면 DB에 인서트
     @Insert("INSERT INTO liked (spotify_id,music_id,title,artist,albumImage,duration) VALUES (#{spotifyId},#{musicId},#{title},#{artist},#{albumImage},#{duration})")
     int insertLike(LikeVO vo);
-
-    @Insert("""
-        INSERT INTO likes (
-            spotify_id,
-            music_id
-        )
-        VALUES (
-            #{spotifyId},
-            #{musicId}
-        )
-    """)
-    int insertLike(LikeVO likeVO);
-
-    @Delete("""
-        DELETE FROM likes
-        WHERE spotify_id = #{spotifyId}
-          AND music_id = #{musicId}
-    """)
-    int deleteLike(
-            @Param("spotifyId") String spotifyId,
-            @Param("musicId") String musicId
-    );
+    // 3. 이미 가입된 사용자라면 정보를 최신으로 업데이트
+    @Delete("Delete FROM liked WHERE spotify_id = #{spotifyId} AND music_id=#{musicId}")
+    int deleteLike(String spotifyId , String musicId);
 }
