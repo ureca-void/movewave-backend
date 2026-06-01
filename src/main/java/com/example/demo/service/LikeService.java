b06ad01ed2509281e1d29fd67cd86b7927df12cc
HaeJun <haejunbag131@gmail.com>
2026-06-01 13:51:07 +0900

    Feat: latest 더미 데이터 추가

--- src/main/java/com/example/demo/service/LikeService.java
+++ src/main/java/com/example/demo/service/LikeService.java
@@ -4,10 +4,12 @@ import com.example.demo.mapper.LikeMapper;
 import com.example.demo.vo.LikeVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Service
 public class LikeService {
+
     @Autowired
     private LikeMapper likeMapper;
 
@@ -15,24 +17,26 @@ public class LikeService {
         return likeMapper.findLike(spotifyId);
     }
 
+    // 좋아요 추가
     public boolean add(LikeVO vo) {
         LikeVO existing = likeMapper.isLike(vo.getSpotifyId(), vo.getMusicId());
-        if(existing == null) {
-            return likeMapper.insertLike(vo)>0;
-        }else{
-            return false;
+
+        if (existing == null) {
+            return likeMapper.insertLike(vo) > 0;
         }
-    }
-    public boolean exists(LikeVO vo) {
-        LikeVO existing = likeMapper.isLike(vo.getSpotifyId(), vo.getMusicId());
-        if(existing == null) {
-            return likeMapper.insertLike(vo)>0;
-        }else{
-            return false;
-        }
+
+        return false;
     }
 
+    // 좋아요 여부 확인
+    public boolean exists(LikeVO vo) {
+        LikeVO existing = likeMapper.isLike(vo.getSpotifyId(), vo.getMusicId());
+
+        return existing != null;
+    }
+
+    // 좋아요 삭제
     public boolean remove(String spotifyId, String musicId) {
-        return likeMapper.deleteLike(spotifyId,musicId) > 0;
+        return likeMapper.deleteLike(spotifyId, musicId) > 0;
     }
 }
